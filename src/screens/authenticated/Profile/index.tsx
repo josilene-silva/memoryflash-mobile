ca3babe6aa046ddab050e836f687346b92434b76
Josilene Vitoria dos Santos da Silva <josilenevitoriasilva@gmail.com>
2022-04-18 11:55:02 -0400

    feat: add building image

--- src/screens/authenticated/Profile/index.tsx
+++ src/screens/authenticated/Profile/index.tsx
@@ -1,5 +1,36 @@
+/* eslint-disable global-require */
 import React from 'react';
+import { Image, View } from 'react-native';
+
+import { Text } from 'src/components/Text';
 
 export function Profile() {
-  return <></>;
+  return (
+    <View
+      style={{
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+      }}
+    >
+      <Image
+        style={{
+          maxHeight: 400,
+          maxWidth: 300,
+          resizeMode: 'contain',
+        }}
+        source={require('src/assets/images/building.png')}
+      />
+      <Text
+        variant={{
+          fontFamily: 'poppins_medium',
+          fontSize: 20,
+          color: 'primary',
+        }}
+      >
+        {'<'} Em construção {'/>'}
+      </Text>
+    </View>
+  );
 }
