a86f388a4ad292643bff3a22f1c423612226b5e9
Josilene Vitoria dos Santos da Silva <josilenevitoriasilva@gmail.com>
2022-05-09 14:46:16 -0400

    refactor: calls LoaderRequest and Modal componets in ListSet screen

--- src/screens/authenticated/Sets/screens/ListSet/index.tsx
+++ src/screens/authenticated/Sets/screens/ListSet/index.tsx
@@ -1,22 +1,25 @@
 import React, { useState, useCallback, useMemo } from 'react';
-import { Alert } from 'react-native';
+import { Alert, View } from 'react-native';
 import { useFocusEffect } from '@react-navigation/native';
-import { useTheme } from 'styled-components';
 
 import { IRouterProps } from 'src/routes/navigation';
 
 import { RoundButton } from 'src/components/RoundButton';
 import { CardLayout } from 'src/components/Layout/Card';
 import { CardsCard } from 'src/components/CardsCard';
+import { LoaderRequest } from 'src/components/LoaderRequest';
+import { IModalData, Modal } from 'src/components/Modal';
+import { Icon } from 'src/components/Icon';
 
 import { ISet } from 'src/data/types';
 
 import { api } from 'src/services/api';
 
+import { ErrorType, getError } from 'src/utils/error';
+
 import {
   Container,
   Scroll,
-  Icon,
   IconContainer,
   IconItem,
   CardsList,
@@ -27,30 +30,36 @@ import {
   AddCard,
 } from './styles';
 
+type ISetData = Omit<ISet, 'practices'>;
+
 export function ListSet({ route, navigation }: IRouterProps) {
   const id = route.params?.id as string;
 
-  const theme = useTheme();
-  const [set, setSet] = useState<Omit<ISet, 'practices'>>({
-    id: '',
-    name: '',
-    description: '',
-    category: {
-      name: '',
-    },
-    cards: [
-      {
-        id: '',
-        front: '',
-        back: '',
-        difficultyLevel: 0,
-      },
-    ],
+  const [set, setSet] = useState<ISetData>({} as ISetData);
+
+  const [loadingRequest, setLoadingRequest] = useState(false);
+  const [modalResponseData, setModalResponseData] = useState<IModalData>({
+    type: 'attention',
+    message: '',
+    isVisible: false,
   });
 
   async function loadSet() {
-    const { data } = await api.get(`/sets/${id}`);
-    setSet(data);
+    try {
+      setLoadingRequest(true);
+      const { data } = await api.get(`/sets/${id}`);
+      setSet(data);
+    } catch (err) {
+      const error = getError(err as ErrorType);
+
+      setModalResponseData({
+        type: error.type,
+        message: error.message,
+        isVisible: true,
+      });
+      setSet({} as ISetData);
+    }
+    setLoadingRequest(false);
   }
 
   useFocusEffect(
@@ -60,40 +69,52 @@ export function ListSet({ route, navigation }: IRouterProps) {
   );
 
   const header = useMemo(
-    () => (
-      <>
-        <IconContainer>
-          <IconItem>
-            <Icon color={theme.colors.gray_medium} size={24} name="trash" />
+    () =>
+      JSON.stringify(set) === '{}' ? (
+        <View />
+      ) : (
+        <>
+          <IconContainer>
+            <IconItem>
+              <Icon color="gray_medium" size={24} name="trash" />
+            </IconItem>
+
+            <IconItem>
+              <Icon color="text_dark" size={24} name="edit-3" />
+            </IconItem>
+          </IconContainer>
+
+          <CardLayout style={{ marginTop: 30 }}>
+            <Category>{set?.category?.name}</Category>
+
+            <About>Sobre</About>
+            <Description>{set?.description}</Description>
+          </CardLayout>
+
+          <IconItem
+            style={{ marginVertical: 29 }}
+            onPress={() => navigation.navigate('CreateCard', { setId: set.id })}
+          >
+            <Icon color="primary" size={32} name="plus" />
+            <AddCard>Adicionar cartão</AddCard>
           </IconItem>
-
-          <IconItem>
-            <Icon color={theme.colors.text_dark} size={24} name="edit-3" />
-          </IconItem>
-        </IconContainer>
-
-        <CardLayout style={{ marginTop: 30 }}>
-          <Category>{set.category.name}</Category>
-
-          <About>Sobre</About>
-          <Description>{set.description}</Description>
-        </CardLayout>
-
-        <IconItem
-          style={{ marginVertical: 29 }}
-          onPress={() => navigation.navigate('CreateCard', { setId: set.id })}
-        >
-          <Icon color={theme.colors.primary} size={32} name="plus" />
-          <AddCard>Adicionar cartão</AddCard>
-        </IconItem>
-      </>
-    ),
-    [navigation, set, theme],
+        </>
+      ),
+    [navigation, set],
   );
 
   return (
     <Scroll>
       <Container>
+        <LoaderRequest visible={loadingRequest} />
+        <Modal
+          type={modalResponseData.type}
+          message={modalResponseData.message}
+          visible={modalResponseData.isVisible}
+          onPress={() =>
+            setModalResponseData(old => ({ ...old, isVisible: false }))
+          }
+        />
         <CardsList
           ListHeaderComponent={() => header}
           data={set.cards}
