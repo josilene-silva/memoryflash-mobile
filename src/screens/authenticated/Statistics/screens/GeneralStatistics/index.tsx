b271833d7f8ea3a8a57918a2f79be4f5e6d66004
Josilene Vitoria dos Santos da Silva <josilenevitoriasilva@gmail.com>
2022-05-09 17:01:01 -0400

    refactor: calls LoaderRequest and Modal componets in GeneralStatistics screen

--- src/screens/authenticated/Statistics/screens/GeneralStatistics/index.tsx
+++ src/screens/authenticated/Statistics/screens/GeneralStatistics/index.tsx
@@ -3,12 +3,16 @@ import { useFocusEffect } from '@react-navigation/native';
 
 import { BarChart } from 'src/components/BarChart';
 import { DataLine } from 'src/components/DataLine';
+import { LoaderRequest } from 'src/components/LoaderRequest';
+import { IModalData, Modal } from 'src/components/Modal';
 
 import { IRouterProps } from 'src/routes/navigation';
 
 import { IPractice } from 'src/data/types/IPractice';
 
 import { api } from 'src/services/api';
+
+import { ErrorType, getError } from 'src/utils/error';
 
 import { Container, Scroll, InformationContainer, Data } from './styles';
 
@@ -18,6 +22,14 @@ interface IPracticeData extends IPractice {
 
 export function GeneralStatistics({ navigation, route }: IRouterProps) {
   const id = route.params?.id;
+
+  const [loadingRequest, setLoadingRequest] = useState(false);
+
+  const [modalResponseData, setModalResponseData] = useState<IModalData>({
+    type: 'attention',
+    message: '',
+    isVisible: false,
+  });
 
   const [practice, setPractice] = useState<IPracticeData>({
     amountEasy: 0,
@@ -35,6 +47,7 @@ export function GeneralStatistics({ navigation, route }: IRouterProps) {
 
   async function loadPractice() {
     try {
+      setLoadingRequest(true);
       const { data } = await api.get(`/sets/${id}`);
       const { practices } = data;
 
@@ -44,7 +57,7 @@ export function GeneralStatistics({ navigation, route }: IRouterProps) {
         amountHard: 0,
       };
 
-      practices.forEach(elem => {
+      practices.forEach((elem: IPracticeData) => {
         amount.amountEasy += elem.amountEasy;
         amount.amountMedium += elem.amountMedium;
         amount.amountHard += elem.amountHard;
@@ -56,7 +69,16 @@ export function GeneralStatistics({ navigation, route }: IRouterProps) {
         amountHard: amount.amountHard,
         timesPracticed: practices.length,
       });
-    } catch (error) {}
+    } catch (err) {
+      const error = getError(err as ErrorType);
+
+      setModalResponseData({
+        type: error.type,
+        message: error.message,
+        isVisible: true,
+      });
+    }
+    setLoadingRequest(false);
   }
 
   useFocusEffect(
@@ -72,6 +94,15 @@ export function GeneralStatistics({ navigation, route }: IRouterProps) {
   return (
     <Scroll>
       <Container>
+        <LoaderRequest visible={loadingRequest} />
+        <Modal
+          type={modalResponseData.type}
+          message={modalResponseData.message}
+          visible={modalResponseData.isVisible}
+          onPress={() =>
+            setModalResponseData(old => ({ ...old, isVisible: false }))
+          }
+        />
         <BarChart
           data={{
             ...practice,
@@ -103,11 +134,6 @@ export function GeneralStatistics({ navigation, route }: IRouterProps) {
             field="Praticado"
             label={`${practice.timesPracticed} vez(es)`}
           />
-          {/* <DataLine
-            type="title"
-            field="Tempo total"
-            label={`${practice.totalTime} minutos`}
-          /> */}
         </InformationContainer>
       </Container>
     </Scroll>
